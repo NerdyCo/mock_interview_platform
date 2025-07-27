c8e5eee396e6ac6b74c7fcdeb2091d7d6b6147fd
NerdyCo <kautsarteguh2002@gmail.com>
2025-07-28 06:35:45 +0700

    firebase setup

---
+++ firebase/client.ts
@@ -0,0 +1,19 @@
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getAuth } from "firebase-admin/auth";
+import { getFirestore } from "firebase-admin/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCNiHdrh6m6zhcSVNyKI34gREFBM9x84aQ",
+  authDomain: "prepwise-c9d6e.firebaseapp.com",
+  projectId: "prepwise-c9d6e",
+  storageBucket: "prepwise-c9d6e.firebasestorage.app",
+  messagingSenderId: "118708595137",
+  appId: "1:118708595137:web:24c348244d4546033e6740",
+  measurementId: "G-ZGM7RQ9FQT",
+};
+
+// Initialize Firebase
+const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
